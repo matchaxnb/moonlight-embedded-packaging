b35d295ddbdd58e2e9188b745fb4e6d4e356ff3e
Matcha <matchalunatic@gmail.com>
2026-07-23 18:22:53 +0200

    fix(dh): manage M.m.p.t versions

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -11,26 +11,32 @@ cd moonlight-embedded
 git checkout --quiet $COMMIT
 git submodule update --quiet --init --recursive
 
-VERSION=$(grep project\( CMakeLists.txt | cut -d ' ' -f 3)
+if [ -x scripts/version.sh ]; then
+  FULL_VERSION=$(bash scripts/version.sh)
+else
+  FULL_VERSION=$(grep project\( CMakeLists.txt | cut -d ' ' -f 3)
+fi
+# Debian package version must be major.minor.patch (no tweak component)
+DEB_VERSION=$(echo "$FULL_VERSION" | grep -Eo '^[0-9]+\.[0-9]+\.[0-9]+')
 
 mkdir /opt/build
-/opt/scripts/git-archive-all.sh --format tar.gz /opt/build/moonlight-embedded_$VERSION.orig.tar.gz
+/opt/scripts/git-archive-all.sh --format tar.gz /opt/build/moonlight-embedded_$FULL_VERSION.orig.tar.gz
 
 cd /opt/build
-mkdir moonlight-embedded-$VERSION
-cd moonlight-embedded-$VERSION
-tar xf ../moonlight-embedded_$VERSION.orig.tar.gz
+mkdir moonlight-embedded-$FULL_VERSION
+cd moonlight-embedded-$FULL_VERSION
+tar xf ../moonlight-embedded_$FULL_VERSION.orig.tar.gz
 
 cp -r /opt/debian .
 cd debian
 sed -i "s/^Maintainer:.*/Maintainer: ${DEBFULLNAME} <${DEBEMAIL}>/" control
-dch -v "${VERSION}-1" "New upstream release ${VERSION}"
+dch -v "${DEB_VERSION}-1" "New upstream release ${DEB_VERSION}"
 cd ..
 
 DEB_BUILD_OPTIONS=terse debuild -us -uc 2>&1 | grep -v -E '^(dh_|make\[|make:.*Entering|make:.*Leaving|/usr/bin/cmake|cd obj-|cmake |gmake|gcc |g\+\+ )' | cat
 
 cd /opt/build
 shopt -s extglob
-cp -v -r !(moonlight-embedded-$VERSION) /out
+cp -v -r !(moonlight-embedded-$FULL_VERSION) /out
 
 echo "Build successful!"
